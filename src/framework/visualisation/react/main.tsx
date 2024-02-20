8e1bea5e447bea986b3c6a3440cdb11371be1745
Kasper Welbers <kasperwelbers@gmail.com>
2024-02-20 13:39:48 +0100

    max width

--- src/framework/visualisation/react/main.tsx
+++ src/framework/visualisation/react/main.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react'
+import * as React from "react";
 
 interface MainProps {
-  elements: JSX.Element[]
+  elements: JSX.Element[];
 }
 
 export const Main = ({ elements }: MainProps): JSX.Element => {
-  elements = elements.map((element, index) => { return { ...element, key: `${index}` } })
+  elements = elements.map((element, index) => {
+    return { ...element, key: `${index}` };
+  });
 
-  return (
-    <div className='w-full h-full'>
-      {elements}
-    </div>
-  )
-}
+  return <div className="max-w-7xl w-full h-full">{elements}</div>;
+};
