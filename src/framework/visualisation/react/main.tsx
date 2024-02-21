2e2211eddb2f80ed679ad2680e7b31d57f102af8
Kasper Welbers <kasperwelbers@gmail.com>
2024-02-21 10:10:30 +0100

    embedded vs standalone mode

--- src/framework/visualisation/react/main.tsx
+++ src/framework/visualisation/react/main.tsx
@@ -9,5 +9,21 @@ export const Main = ({ elements }: MainProps): JSX.Element => {
     return { ...element, key: `${index}` };
   });
 
+  if (process.env.REACT_APP_BUILD !== "standalone" && process.env.NODE_ENV === "production") {
+    return <Embedded elements={elements} />;
+  } else {
+    return <Standalone elements={elements} />;
+  }
+};
+
+const Embedded = ({ elements }: MainProps): JSX.Element => {
   return <div className="max-w-7xl w-full h-full">{elements}</div>;
+};
+
+const Standalone = ({ elements }: MainProps): JSX.Element => {
+  return (
+    <div className="p-4 sm:p-8 md:p-12 flex justify-center w-full h-full">
+      <div className="max-w-7xl">{elements}</div>
+    </div>
+  );
 };
